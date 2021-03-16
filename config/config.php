2a743c9b820fc5bd47be5478ef5afaac06d090e5
billiemead <billie@billiemead.com>
2021-03-16 19:36:29 -0400

    3.16.21Update

--- config/config.php
+++ config/config.php
@@ -3,18 +3,29 @@
 https://github.com/billiemead/activeecommerce-form-builder.git
 Licensed under the GNU General Public License v3.0
 Author: Billie Mead (billiemead.com)
-Last Updated: 02/20/2021
+Last Updated: 03/15/2021
 ----------------------*/
 return [
+    
     /**
-     * Url path to use for this package routes
+     * Front-end URL path to use for this package routes
      */
     'url_path' => '/form-builder',
 
     /**
-     * Template layout file. This is the path to the layout file your application uses
+     * Back-end URL path to use for this package routes
      */
-    'layout_file' => 'backend.layouts.app',
+    'url_path_admin' => '/admin/form-builder',
+
+    /**
+     * Front-end Template layout file. This is the path to the layout file your application uses
+     */
+    'layout_file' => 'frontend.layouts.app',
+
+    /**
+     * Back-end Template layout file. This is the path to the layout file your application uses
+     */
+    'layout_file_admin' => 'backend.layouts.app',
 
     /**
      * The stack section in the layout file to output js content
@@ -31,7 +42,7 @@ return [
      * The class that will provide the roles we will display on form create or edit pages?
      */
     
-    'roles_provider' => billiemead\FormBuilder\Services\RolesProvider::class,
+    'roles_provider' => \App\Role::class,
 
     /**
      * Models used in form builder
